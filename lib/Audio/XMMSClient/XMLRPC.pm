463d722964285ad5d993acaaff696b5160ea8c2b
Florian Ragwitz <rafl@debian.org>
2006-10-22 19:04:27 +0000

    Import of FLORA/Audio-XMMSClient-XMLRPC-0.03 from CPAN.
    gitpan-cpan-distribution: Audio-XMMSClient-XMLRPC
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FLORA/Audio-XMMSClient-XMLRPC-0.03.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

--- lib/Audio/XMMSClient/XMLRPC.pm
+++ lib/Audio/XMMSClient/XMLRPC.pm
@@ -26,11 +26,11 @@ Audio::XMMSClient::XMLRPC - XMLRPC interface to xmms2
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
